1c74de9aee212ac68f51a450830400eb8c4be015
qonyo <faizqornijo@gmail.com>
2022-02-12 22:06:43 +0700

    String done

---
+++ Section15JavaScriptStringsandMore/Practice2/index.js
@@ -0,0 +1,5 @@
+// DON'T TOUCH THIS LINE!
+const message = "    TASTE THE RAINBOW!  "; //DON'T CHANGE THIS LINE PLEASE!
+let whisper =  message.trim().toLowerCase();
+
+// YOUR CODE BELOW THIS LINE:
---
+++ Section15JavaScriptStringsandMore/Practice3/index.js
@@ -0,0 +1,5 @@
+// NO TOUCHING! (please)
+const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+let roll = `You rolled a ${die1} and a ${die2}. They sum to ${die1 + die2}`;
+// YOUR CODE BELOW THIS LINE:
