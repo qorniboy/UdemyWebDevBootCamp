e9ce835204fca94f4e60a9039b77bbad532a0a6b
qonyo <faizqornijo@gmail.com>
2022-02-16 13:10:19 +0700

    section 18 and 19

---
+++ Section18JSObjectLiteral/Practice1/index.js
@@ -0,0 +1,8 @@
+// Your code goes in here:
+
+let product = {
+    "name" : "Gummy Bears",
+    "inStock" : true,
+    "price" : 1.99,
+    "flavors" : ["grape", "apple", "cherry"]
+}
---
+++ Section19JSLoop/Practice1/index.js
@@ -0,0 +1,8 @@
+// Print out "Da ba dee da ba daa" 6 times, using a for loop
+
+let toPrint = "Da ba dee da ba daa"
+
+
+for (let i = 0; i < 6; i++) {
+  console.log(toPrint);
+}
